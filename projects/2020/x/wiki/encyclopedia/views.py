9f2705542d26d71e2b3b70f56bb3ba71a0212b0d
Carina Calixto Ribeiro de Araujo <carinacalixto@gmail.com>
2022-10-25 00:47:53 -0300

    Project1 - Entry, Index and Search

--- projects/2020/x/wiki/encyclopedia/views.py
+++ projects/2020/x/wiki/encyclopedia/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, render
+import markdown2
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def entry(request, title):
+    
+    if util.get_entry(title):
+        return render(request, f"encyclopedia/entry.html", {
+            "content": markdown2.markdown(util.get_entry(title)),
+            "title": title
+        })
+    else:
+        return render(request, f"encyclopedia/error.html")
+    
+def search(request):
+    query = request.POST.get('q', 'notfound')
+    if util.get_entry(query):
+        return render(request, f"encyclopedia/entry.html", {
+            "content": markdown2.markdown(util.get_entry(query)),
+            "title": query
+        })
+    else:
+        return render(request, f"encyclopedia/results.html", {
+            "all_entries": util.list_entries(),
+            "query": query
+        })
 
+
+# def search(request):
+#     entries = util.list_entries()
+#     find_entries = []
+#     # find_entries = list()
+#     search_box = request.POST.get("q", "").capitalize()
+
+#     if search_box in entries:
+#         return HttpResponseRedirect(f"wiki/{search_box}")
+        
+#     for entry in entries:
+#         if search_box in entry:
+#             find_entries.append(entry)
+#         else:
+#             print(f'{find_entries}')
+#     if find_entries:
+#         return render(request, "encyclopedia/search.html", {
+#           "search_result": find_entries,
+#           "search": search_box
+#     })
+#     else:
+#         return render(request, "encyclopedia/search.html", {"no_result": f"No results for {search_box}"})
